e94e8e7941dc581306c385a9a6b8c9d507a0205c
jjgorton <jjgorton@gmail.com>
2019-04-15 19:46:20 -0400

    receiving data from api and rendering map

--- src/reducers/mapReducer.js
+++ src/reducers/mapReducer.js
@@ -1,11 +1,32 @@
-//action types here
+import { GET_DATA_START, GET_DATA_SUCCESS, GET_DATA_FAILURE } from '../actions';
 
 const initialState = {
-	data : []
+	data    : [],
+	loading : false,
+	error   : null
 };
 
 export const mapReducer = (state = initialState, action) => {
 	switch (action.type) {
+		case GET_DATA_START:
+			return {
+				...state,
+				loading : true
+			};
+		case GET_DATA_SUCCESS:
+			return {
+				...state,
+				loading : false,
+				data    : action.payload,
+				error   : null
+			};
+		case GET_DATA_FAILURE:
+			return {
+				...state,
+				loading : false,
+				error   : action.payload
+			};
+
 		default:
 			return state;
 	}
